3a062781992f1e670a3ee7e04bfaa2e62f892d85
Matthieu FOURNET <matthieu.fournet@orange.com>
2023-03-08 16:05:31 +0100

    COSMETICS

--- savePageAs.sh
+++ savePageAs.sh
@@ -5,14 +5,14 @@ set -u
 set -o pipefail
 
 
-waitTimeSeconds_load=4
-waitTimeSeconds_save=8
+waitTimeSecondsLoad=4
+waitTimeSecondsSave=8
 scriptname="$(basename "$0")"
 destination='.'
 browser='google-chrome'
 suffix=''
 url=''
-
+webBrowserWindowId=''
 
 checkXdotoolIsInstalled() {
 	if ! xdotool --help &>/dev/null; then
@@ -25,19 +25,19 @@ checkXdotoolIsInstalled() {
 print_usage() {
 	cat <<-EOF
 
-	${scriptname}: Open the given url in a browser tab/window, perform 'Save As' operation and close the tab/window.
+	$scriptname: Open the given url in a browser tab/window, perform 'Save As' operation and close the tab/window.
 
 	USAGE:
-	    ${scriptname} URL [OPTIONS]
+	    $scriptname URL [OPTIONS]
 
 	URL                      The url of the web page to be saved.
 
 	options:
-	  -d, --destination      Destination path. If a directory, then file is saved with default name inside the directory, else assumed to be full path of target file. Default = '${destination}'
+	  -d, --destination      Destination path. If a directory, then file is saved with default name inside the directory, else assumed to be full path of target file. Default = '$destination'
 	  -s, --suffix           An optional suffix string for the target file name (ignored if --destination arg is a full path)
-	  -b, --browser          Browser executable to be used (must be one of 'google-chrome', 'chromium-browser' or 'firefox'). Default = '${browser}'.
-	  --load-wait-time       Number of seconds to wait for the page to be loaded (i.e., seconds to sleep before Ctrl+S is 'pressed'). Default = ${waitTimeSeconds_load}
-	  --save-wait-time       Number of seconds to wait for the page to be saved (i.e., seconds to sleep before Ctrl+F4 is 'pressed'). Default = ${waitTimeSeconds_save}
+	  -b, --browser          Browser executable to be used (must be one of 'google-chrome', 'chromium-browser' or 'firefox'). Default = '$browser'.
+	  --load-wait-time       Number of seconds to wait for the page to be loaded (i.e., seconds to sleep before Ctrl+S is 'pressed'). Default = $waitTimeSecondsLoad
+	  --save-wait-time       Number of seconds to wait for the page to be saved (i.e., seconds to sleep before Ctrl+F4 is 'pressed'). Default = $waitTimeSecondsSave
 	  -h, --help             Display this help message and exit.\n
 	EOF
 	}
@@ -47,7 +47,7 @@ print_usage() {
 # (Inspiration: http://stackoverflow.com/a/13596664/1857518)
 has_non_printable_or_non_ascii() {
 	LANG=C
-	if printf "%s" "${1}" | grep '[^ -~]\+' &>/dev/null; then
+	if printf "%s" "$1" | grep '[^ -~]\+' &>/dev/null; then
 		printf 1
 	else
 		printf 0
@@ -56,41 +56,41 @@ has_non_printable_or_non_ascii() {
 
 
 validate_input() {
-	if [[ -z "${url}" ]]; then
+	if [[ -z "$url" ]]; then
 		printf "ERROR: URL must be specified." >&2
 		print_usage
 		exit 1
 	fi
 
-	if [[ -d "${destination}" ]]; then
-		printf "INFO: The specified destination ('%s') is a directory path, will save file inside it with the default name.\n" "${destination}">&2
+	if [[ -d "$destination" ]]; then
+		printf "INFO: The specified destination ('%s') is a directory path, will save file inside it with the default name.\n" "$destination">&2
 	else
-		local basedir="$(dirname "${destination}")"
-		if [[ ! -d "${basedir}" ]]; then
-			printf "ERROR: Directory '%s' does not exist - Will NOT continue.\n" "${basedir}" >&2
+		local basedir="$(dirname "$destination")"
+		if [[ ! -d "$basedir" ]]; then
+			printf "ERROR: Directory '%s' does not exist - Will NOT continue.\n" "$basedir" >&2
 			exit 1
 		fi
 	fi
 	destination="$(readlink -f "$destination")"	# Ensure absolute path
 
-	if [[ "${browser}" != "google-chrome" && "${browser}" != "chromium-browser" && "${browser}" != "firefox" ]]; then
-		printf "ERROR: Browser (%s) is not supported, must be one of 'google-chrome', 'chromium-browser' or 'firefox'.\n" "${browser}" >&2
+	if [[ "$browser" != 'google-chrome' && "$browser" != 'chromium-browser' && "$browser" != 'firefox' ]]; then
+		printf "ERROR: Browser (%s) is not supported, must be one of 'google-chrome', 'chromium-browser' or 'firefox'.\n" "$browser" >&2
 		exit 1
 	fi
 
-	if ! command -v "${browser}" &>/dev/null; then
-		printf "ERROR: Command '${browser}' not found. Make sure it is installed, and in path.\n" >&2
+	if ! command -v "$browser" &>/dev/null; then
+		printf "ERROR: Command '$browser' not found. Make sure it is installed, and in path.\n" >&2
 		exit 1
 	fi
 
 	local num_regexp='^.[0-9]+$|^[0-9]+$|^[0-9]+.[0-9]+$'	# Matches a valid number (in decimal notation)
-	if [[ ! "${waitTimeSeconds_load}" =~ $num_regexp || ! "${waitTimeSeconds_save}" =~ $num_regexp ]]; then
-		printf "ERROR: --load-wait-time (='%s'), and --waitTimeSeconds_save(='%s') must be valid numbers.\n" "${waitTimeSeconds_load}" "${waitTimeSeconds_load}" >&2
+	if [[ ! "$waitTimeSecondsLoad" =~ $num_regexp || ! "$waitTimeSecondsSave" =~ $num_regexp ]]; then
+		printf "ERROR: --load-wait-time (='%s'), and --waitTimeSeconds_save(='%s') must be valid numbers.\n" "$waitTimeSecondsLoad" "$waitTimeSecondsLoad" >&2
 		exit 1
 	fi
 
-	if [[ $(has_non_printable_or_non_ascii "${destination}") -eq 1 || $(has_non_printable_or_non_ascii "${suffix}") -eq 1 ]]; then
-		printf "ERROR: Either --destination ('%s') or --suffix ('%s') contains a non ascii or non-printable ascii character(s). " "${destination}" "${suffix}" >&2
+	if [[ $(has_non_printable_or_non_ascii "$destination") -eq 1 || $(has_non_printable_or_non_ascii "$suffix") -eq 1 ]]; then
+		printf "ERROR: Either --destination ('%s') or --suffix ('%s') contains a non ascii or non-printable ascii character(s). " "$destination" "$suffix" >&2
 		printf "'xdotool' does not mingle well with non-ascii characters (https://code.google.com/p/semicomplete/issues/detail?id=14).\n\n" >&2
 		printf '!!!! Will NOT proceed !!!!\n' >&2
 		exit 1
@@ -109,19 +109,19 @@ debug() {
 
 loadPageInBrowser() {
 	"$browser" "$url" &>/dev/null &
-	sleep "$waitTimeSeconds_load"
+	sleep "$waitTimeSecondsLoad"
 	debug 'Done : Launch browser + wait for page to load'
 	}
 
 
 findBrowserWindowId() {
-	browser_wid="$(xdotool search --sync --onlyvisible --class "${browser}" | head -n 1)"
-	wid_re='^[0-9]+$'	# window-id must be a valid integer
-	if [[ ! "${browser_wid}" =~ ${wid_re} ]]; then
+	webBrowserWindowId="$(xdotool search --sync --onlyvisible --class "$browser" | head -n 1)"
+	windowIdValidationRegex='^[0-9]+$'	# window-id must be a valid integer
+	if [[ ! "$webBrowserWindowId" =~ $windowIdValidationRegex ]]; then
 		printf "ERROR: Unable to find X-server window id for browser.\n" >&2
 		exit 1
 	fi
-	debug "Done : find the ID of the browser window : '$browser_wid'"
+	debug "Done : find the ID of the browser window : '$webBrowserWindowId'"
 	}
 
 
@@ -148,12 +148,12 @@ main() {
 				;;
 			--load-wait-time)
 				shift;
-				waitTimeSeconds_load="$1"
+				waitTimeSecondsLoad="$1"
 				shift
 				;;
 			--save-wait-time)
 				shift;
-				waitTimeSeconds_save="$1"
+				waitTimeSecondsSave="$1"
 				shift
 				;;
 			-h | --help)
@@ -161,13 +161,13 @@ main() {
 				exit 0
 				;;
 			-*)
-				printf "ERROR: Unknown option: %s\n" "${1}">&2
+				printf "ERROR: Unknown option: %s\n" "$1">&2
 				print_usage
 				exit 1
 				;;
 			*)
 				if [ ! -z "$url" ]; then
-					printf "ERROR: Expected exactly one positional argument (URL) to be present, but encountered a second one ('%s').\n\n" "${1}" >&2
+					printf "ERROR: Expected exactly one positional argument (URL) to be present, but encountered a second one ('%s').\n\n" "$1" >&2
 					print_usage
 					exit 1
 				fi
@@ -181,41 +181,41 @@ main() {
 	loadPageInBrowser
 	findBrowserWindowId
 
-	# Activate the ${browser} window, and "press" ctrl+s
+	# Activate the "$browser" window, and "press" ctrl+s
 	#TODO:
-	#xdotool windowactivate "${browser_wid}" key --clearmodifiers "ctrl+s"
-	xdotool windowactivate "${browser_wid}"
-	xdotool key --window "${browser_wid}" --clearmodifiers "ctrl+s"
+	#xdotool windowactivate "$webBrowserWindowId" key --clearmodifiers "ctrl+s"
+	xdotool windowactivate "$webBrowserWindowId"
+	xdotool key --window "$webBrowserWindowId" --clearmodifiers "ctrl+s"
 
 
 	sleep 1 # Give 'Save as' dialog box time to show up
 
 	# Resolve the expected title name for save file dialog box (chrome & firefox differ in this regard)
-	if [[ "${browser}" == "firefox" ]]; then
-		savefile_dialog_title="Save as"
+	if [[ "$browser" == 'firefox' ]]; then
+		savefile_dialog_title='Save as'
 	else
-		savefile_dialog_title="Save file"
+		savefile_dialog_title='Save file'
 	fi
 	# Find window id for the "Save file" dialog box
 	savefile_wid="$(xdotool search --name "$savefile_dialog_title" | head -n 1)"
-	if [[ ! "${savefile_wid}" =~ ${wid_re} ]]; then
+	if [[ ! "$savefile_wid" =~ $windowIdValidationRegex ]]; then
 		printf "ERROR: Unable to find window id for 'Save File' Dialog.\n" >&2
 		exit 1
 	fi
 
 	# Fix for Issue #1: Explicitly focus on the "name" field (works on both: gnome, and kde)
-	xdotool windowactivate "${savefile_wid}" key --delay 20 --clearmodifier "Alt+n"
+	xdotool windowactivate "$savefile_wid" key --delay 20 --clearmodifier 'Alt+n'
 
 	# Check if we are using kde
-	is_kde=0
+	isKde=0
 	# Don't feel bad if DESKTOP_SESSION env variable is not present
 	set +u
-	if [[ "${DESKTOP_SESSION}" =~ ^kde-? ]]; then
-		is_kde=1
+	if [[ "$DESKTOP_SESSION" =~ ^kde-? ]]; then
+		isKde=1
 	fi
 	set -u
 
-	if [[ ! -z "${suffix}" ]]; then
+	if [[ ! -z "$suffix" ]]; then
 		###########################
 		# Make sure that we are at correct position before typing the "suffix"
 		#
@@ -225,49 +225,49 @@ main() {
 		# Now this strategy is certainly not full proof and assumes that file extension is always 4 characters long ('html'),
 		# but this is the only fix I can think for this special case right now. Of course it's easy to tweak the number of
 		# Left key moves you need if you know your file types in advance.
-		if [[ "${is_kde}" -eq 1 ]]; then
-			printf "INFO: Desktop session is found to be '${DESKTOP_SESSION}', hence the full file name will be highlighted. " >&2
+		if [[ "$isKde" -eq 1 ]]; then
+			printf "INFO: Desktop session is found to be '$DESKTOP_SESSION', hence the full file name will be highlighted. " >&2
 			printf "Assuming extension .html to move back 5 character left before adding suffix (change accordingly if you need to).\n" >&2
-			xdotool windowactivate "${savefile_wid}" key --delay 40 --clearmodifier End Left Left Left Left Left
+			xdotool windowactivate "$savefile_wid" key --delay 40 --clearmodifier End Left Left Left Left Left
 		else
-			xdotool windowactivate "${savefile_wid}" key --delay 20 --clearmodifiers Right
+			xdotool windowactivate "$savefile_wid" key --delay 20 --clearmodifiers Right
 		fi
 		set -u
 		###########################
 
-		extraarg=""
-		if [[ "${suffix::1}}" == "-" ]]; then
-			extraarg="-"
+		extraarg=''
+		if [[ "${suffix::1}" == '-' ]]; then
+			extraarg='-'
 		fi
-		xdotool type --delay 10 --clearmodifiers "${extraarg}" "${suffix}"
+		xdotool type --delay 10 --clearmodifiers "$extraarg" "$suffix"
 	fi
 
-	# Activate the 'Save File' dialog and type in the appropriate filename (depending on ${destination} value: 1) directory, 2) full path, 3) empty)
-	if [[ ! -z "${destination}" ]]; then
-		if [[ -d "${destination}" ]]; then
+	# Activate the 'Save File' dialog and type in the appropriate filename (depending on $destination value: 1) directory, 2) full path, 3) empty)
+	if [[ ! -z "$destination" ]]; then
+		if [[ -d "$destination" ]]; then
 			# Case 1: --destination was a directory.
-			xdotool windowactivate "${savefile_wid}" key --delay 20 --clearmodifiers Home
-			xdotool type --delay 10 --clearmodifiers "${destination}/"
+			xdotool windowactivate "$savefile_wid" key --delay 20 --clearmodifiers Home
+			xdotool type --delay 10 --clearmodifiers "$destination/"
 		else
 			# Case 2: --destination was full path.
-			xdotool windowactivate "${savefile_wid}" key --delay 20 --clearmodifiers "ctrl+a" "BackSpace"
-			xdotool type --delay 10 --clearmodifiers "${destination}"
+			xdotool windowactivate "$savefile_wid" key --delay 20 --clearmodifiers "ctrl+a" "BackSpace"
+			xdotool type --delay 10 --clearmodifiers "$destination"
 		fi
 	fi
-	xdotool windowactivate "${savefile_wid}" key --delay 20 --clearmodifiers Return
+	xdotool windowactivate "$savefile_wid" key --delay 20 --clearmodifiers Return
 
 	printf "INFO: Saving web page ...\n" >&2
 
 	# Wait for the file to be completely saved
-	sleep ${waitTimeSeconds_save}
+	sleep "$waitTimeSecondsSave"
 
 	# Close the browser tab/window (Ctrl+w for KDE, Ctrl+F4 otherwise)
-	if [[ "${is_kde}" -eq 1 ]]; then
-		xdotool windowactivate "${browser_wid}" key --clearmodifiers "ctrl+w"
+	if [[ "$isKde" -eq 1 ]]; then
+		xdotool windowactivate "$webBrowserWindowId" key --clearmodifiers "ctrl+w"
 	else
 	# TODO:
-	#	xdotool windowactivate "${browser_wid}" key --clearmodifiers "ctrl+F4"
-		xdotool windowactivate "${browser_wid}" key --clearmodifiers "ctrl+w"
+	#	xdotool windowactivate "$webBrowserWindowId" key --clearmodifiers "ctrl+F4"
+		xdotool windowactivate "$webBrowserWindowId" key --clearmodifiers "ctrl+w"
 	fi
 	printf "INFO: Done!\n">&2
 	}
